8f18721d8b1b36578779017f11befd30bc71d0fe
Chris Ernst <cernst3@gatech.edu>
2011-07-14 12:04:51 -0400

    first commit

---
+++ jerryjava.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+	var ksjld = "tr";	
+	var dh = "@";
+	var djhklf = "du";
+	var lkjf = "cernst3";
+	var sdkljh = "gate"
+	var skfh = ".";
+	sdkljh += "ch";
+	skfh = skfh + "e";
+	$("email").innerHTML = "<a href='mailto:"+lkjf+dh+sdkljh+skfh+djhklf+"?Subject=Hello%20Jerry!'>Get in touch with me</a>";
+}
